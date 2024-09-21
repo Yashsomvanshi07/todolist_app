c91efc9efad19b0ec715f9866b52d2ceff3fa8b5
YASH SOMVANSHI <134855246+Yashsomvanshi07@users.noreply.github.com>
2024-09-21 13:29:56 +0530

    project urls

--- urls.py
+++ urls.py
@@ -1,9 +1,7 @@
-from django.urls import path
-from .views import todolist, edit_task, delete_task, toggle_task
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', todolist, name='todolist'),
-    path('edit_task/<int:task_id>/', edit_task, name='edit_task'),
-    path('delete_task/<int:task_id>/', delete_task, name='delete_task'),
-    path('toggle_task/<int:task_id>/', toggle_task, name='toggle_task'),  # Add this URL for toggling task completion
+    path('admin/', admin.site.urls),
+    path('todo/', include('todolist.urls')),
 ]
